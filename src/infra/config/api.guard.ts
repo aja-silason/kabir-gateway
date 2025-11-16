81e8e59ae93d1bafe297217d8f9c3ab4d9b68b3d
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-11-16 06:54:54 +0100

    feat: env

--- src/infra/config/api.guard.ts
+++ src/infra/config/api.guard.ts
@@ -4,20 +4,14 @@ import { Request } from "express";
 @Injectable()
 export class ApiGuard implements CanActivate {
 
-    private validationApiKey = '123456789'
+    private validationApiKey = process.env.API_KEY
 
     canActivate(context: ExecutionContext): boolean {
         const req = context.switchToHttp().getRequest<Request>();
 
         let apiKey = req.headers['x-api-key'] as string | undefined;
 
-        if (!apiKey && req.headers['authorization']) {
-            const authHeader = req.headers['authorization'] as string;
-            apiKey = authHeader.replace(/^Bearer\s+/i, '');    
-        }
-
         if(apiKey?.trim() === this.validationApiKey) {
-            console.log(apiKey);
             return true;
         }
         
