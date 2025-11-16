2ef599bcf090a8d488bbd201899f1c5a34aaa62e
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-11-16 08:45:04 +0100

    feat: adiciona concorrencia no service

--- src/main.ts
+++ src/main.ts
@@ -20,13 +20,15 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   app.enableCors({
-    
-    // em dev
     origin: true,
     credentials: true
   });
 
   app.get<ConfigService>(ConfigService);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${port}`);
+
 }
 bootstrap();
