546efa8658291acdf9ff48d6089dffe3e73b57cd
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-11-16 06:30:36 +0100

    feat: adiciona cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module} from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigureModule } from './config/config.module';
 import { AvailableDriversModule } from './infra/http/available-drivers/available-drivers.module';
 import { DatabaseModule } from './infra/db/database.module';
 import { LogsModule } from './infra/http/logs/logs.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: 10,
+          ttl: 60,  
+        },
+      ],
+      errorMessage: 'Too many requests',
+    }),
+
+    CacheModule.register({
+      ttl: 30,
+      max: 100,
+    }),
+
     ConfigureModule,
     AvailableDriversModule,
     DatabaseModule,
--- src/infra/http/available-drivers/available-drivers.module.ts
+++ src/infra/http/available-drivers/available-drivers.module.ts
@@ -3,10 +3,17 @@ import { AvailableDriversService } from './available-drivers.service';
 import { AvailableDriversController } from './available-drivers.controller';
 import { ListAvailableDriversUsecase } from 'src/app/usecase/driver/list-available-drivers.usecase';
 import { LogsModule } from '../logs/logs.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   controllers: [AvailableDriversController],
-  imports: [LogsModule],
+  imports: [
+    LogsModule,
+    CacheModule.register({
+      ttl: 30, 
+      max: 100,
+    }),
+  ],
   providers: [
     AvailableDriversService,
     ListAvailableDriversUsecase,
